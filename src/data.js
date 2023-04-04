7a7649ebb1745e19216ce9c5bd4ced3a68bdf5f1
Abhishek7487 <dellabhishek7487@gmail.com>
2023-04-04 09:12:44 +0530

    Uploaded new my favourite songs

--- src/data.js
+++ src/data.js
@@ -3,83 +3,83 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-300x300.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      name: "Perfect",
+      cover: "https://i.scdn.co/image/ab67616d00001e02ba5db46f4b838ef6027e6f96",
+      artist: "Ed, Sheeran",
+      audio:
+        "https://drive.google.com/uc?export=download&id=14LYf71FhdGU6nHNwE76rzPSrVc3_kiW8",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
 
     {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-300x300.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF83A9", "#ab417f"],
+      name: "Mohbhanga",
+      cover: "https://i.scdn.co/image/ab67616d0000b273926ddfb42b05a8d6c9fba029",
+      artist: "Rohit Sharma",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1_ZcHdZvH84URmN0q5wfvmujwiqRwkMXd",
+      color: ["#F1B183", "#5893A5"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-300x300.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Keep Going",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-300x300.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9224",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-300x300.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-300x300.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      name: "Shape of You",
+      cover: "https://i.scdn.co/image/ab67616d00001e02ba5db46f4b838ef6027e6f96",
+      artist: "Ed Sheeran",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1qqyAkVJI_jCvCooq6U9RoZpE3mwvAULr",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Jazz Cabbage",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-300x300.jpg",
-      artist: "Ian Ewing, Strehlow",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
-      color: ["#BA4A46", "#FDF0DD"],
+      name: "Believer",
+      cover: "https://i.scdn.co/image/ab67616d00001e025675e83f707f1d7271e5cf8a",
+      artist: "Imagine Dragons",
+      audio:
+        "https://drive.google.com/uc?export=download&id=17f9sf_wiMmSNlOi8QN-6TsxK7JN6lwy9",
+      color: ["#203466", "#5D4754"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Laggons",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-300x300.jpg",
-      artist: "Ian Ewing, Strehlow",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
-      color: ["#BA4A46", "#FDF0DD"],
+      name: "Girls Like You",
+      cover: "https://i.scdn.co/image/ab67616d00001e02deae7d931928fc1543e70203",
+      artist: "Marron 5",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1WELK1c_A_NOz9ZZrArqM3wusxdveW1Mj",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Senorita",
+      cover: "https://i.scdn.co/image/ab67616d00001e02e6095c382c2853667c1623eb",
+      artist: "Shawn Mendes",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1a35snM-jCJ26EJj8SYJNHuIqVrU8PEjB",
+      color: ["#AC2C21", "#E7A77B"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Dandelions",
+      cover: "https://i.scdn.co/image/ab67616d00001e0297e971f3e53475091dc8d707",
+      artist: "Ruth B",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1iYdHFn7WRuOHi08R0iqo-fbtxN7vf1VC",
+      color: ["#FDF0DD", "#464646"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Despasito",
+      cover: "https://i.scdn.co/image/ab67616d00001e02ef0d4234e1a645740f77d59c",
+      artist: "Luis Fonsi",
+      audio:
+        "https://drive.google.com/uc?export=download&id=1zxy3kWgRZVbXrvx-1CF4_b06Yt8MIW8W",
+      color: ["#DCB491", "#151410"],
       id: uuidv4(),
       active: false,
     },
